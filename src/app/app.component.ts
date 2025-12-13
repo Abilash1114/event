b7f5bfdece4df3b718f28e1be9d6788f05029d59
Abilash Ravi <abilashravi09@gmail.com>
2025-12-14 00:32:41 +0530

    night

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,44 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,FooterComponent],
+  imports: [RouterOutlet, HeaderComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'eventManagement';
+export class AppComponent implements AfterViewInit {
+  showLoader = true;
+
+  ngAfterViewInit() {
+    const images = Array.from(document.images);
+    let loadedCount = 0;
+    const totalImages = images.length;
+
+    // ✅ Define first
+    const increment = () => {
+      loadedCount++;
+      if (loadedCount === totalImages) {
+        this.showLoader = false;
+      }
+    };
+
+    if (totalImages === 0) {
+      this.showLoader = false;
+      return;
+    }
+
+    images.forEach((img) => {
+      if (img.complete) {
+        increment();
+      } else {
+        img.addEventListener('load', increment);
+        img.addEventListener('error', increment);
+      }
+    });
+  }
 }
